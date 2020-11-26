7d60c6775481e4d062c3e9d116e10fdc119d7d9b
gon-nexus <ggaliano@nexusgeographics.com>
2020-11-26 13:58:49 +0100

    Columna d'editar e ID ja no son editables, ajustaments pel canvi d'idioma de les variables a l'ag grid

--- dist/sitmun-frontend-gui/data-grid/data-grid.component.d.ts
+++ dist/sitmun-frontend-gui/data-grid/data-grid.component.d.ts
@@ -7,25 +7,25 @@ export declare class DataGridComponent {
     searchValue: string;
     private gridApi;
     private gridColumnApi;
-    columnaEstat: boolean;
+    statusColumn: boolean;
     map: Map<number, number>;
     private params;
     rowData: any[];
-    comptadorCanvis: number;
-    comptadorCanvisAnterior: number;
-    comptadorRedo: number;
-    canviAmbModificacions: boolean;
+    changeCounter: number;
+    previousChangeCounter: number;
+    redoCounter: number;
+    modificationChange: boolean;
     gridOptions: any;
     frameworkComponents: any;
     columnDefs: any[];
     getAll: () => Observable<any>;
-    botoDescartarCanvis: boolean;
-    botoUndo: boolean;
-    botoRedo: boolean;
-    botoAplicarCanvis: boolean;
-    botoElimina: boolean;
-    botoNou: boolean;
-    searchGeneral: boolean;
+    discardChangesButton: boolean;
+    undoButton: boolean;
+    redoButton: boolean;
+    applyChangesButton: boolean;
+    deleteButton: boolean;
+    newButton: boolean;
+    globalSearch: boolean;
     remove: EventEmitter<any[]>;
     new: EventEmitter<number>;
     sendChanges: EventEmitter<any[]>;
@@ -43,7 +43,7 @@ export declare class DataGridComponent {
     onCellEditingStopped(e: any): void;
     onCellValueChanged(params: any): void;
     static ɵfac: ɵngcc0.ɵɵFactoryDef<DataGridComponent, never>;
-    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<DataGridComponent, "app-data-grid", never, { "frameworkComponents": "frameworkComponents"; "columnDefs": "columnDefs"; "getAll": "getAll"; "botoDescartarCanvis": "botoDescartarCanvis"; "botoUndo": "botoUndo"; "botoRedo": "botoRedo"; "botoAplicarCanvis": "botoAplicarCanvis"; "botoElimina": "botoElimina"; "botoNou": "botoNou"; "searchGeneral": "searchGeneral"; }, { "remove": "remove"; "new": "new"; "sendChanges": "sendChanges"; }, never, never>;
+    static ɵcmp: ɵngcc0.ɵɵComponentDefWithMeta<DataGridComponent, "app-data-grid", never, { "frameworkComponents": "frameworkComponents"; "columnDefs": "columnDefs"; "getAll": "getAll"; "discardChangesButton": "discardChangesButton"; "undoButton": "undoButton"; "redoButton": "redoButton"; "applyChangesButton": "applyChangesButton"; "deleteButton": "deleteButton"; "newButton": "newButton"; "globalSearch": "globalSearch"; }, { "remove": "remove"; "new": "new"; "sendChanges": "sendChanges"; }, never, never>;
 }
 
 //# sourceMappingURL=data-grid.component.d.ts.map

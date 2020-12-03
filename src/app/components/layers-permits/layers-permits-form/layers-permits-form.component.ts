9ab83f9daa5e0bace755a62da43b968a2429f2f5
gon-nexus <ggaliano@nexusgeographics.com>
2020-12-03 15:12:40 +0100

    Afegida taula rols a permisos sobre les capes

--- src/app/components/layers-permits/layers-permits-form/layers-permits-form.component.ts
+++ src/app/components/layers-permits/layers-permits-form/layers-permits-form.component.ts
@@ -2,12 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { tick } from '@angular/core/testing';
 import { FormControl, FormGroup, Validators  } from '@angular/forms';
 import {  ActivatedRoute,  Router} from '@angular/router';
-import { CartographyGroupService } from 'dist/sitmun-frontend-core/';
+import { CartographyGroupService, RoleService, Role } from 'dist/sitmun-frontend-core/';
 import { Connection } from 'dist/sitmun-frontend-core/connection/connection.model';
 import { HttpClient } from '@angular/common/http';
 import { UtilsService } from '../../../services/utils.service';
 import { BtnEditRenderedComponent } from 'dist/sitmun-frontend-gui/';
-
 @Component({
   selector: 'app-layers-permits-form',
   templateUrl: './layers-permits-form.component.html',
@@ -15,6 +14,10 @@ import { BtnEditRenderedComponent } from 'dist/sitmun-frontend-gui/';
 })
 export class LayersPermitsFormComponent implements OnInit {
 
+  columnDefs: any[];
+  public frameworkComponents = {
+    btnEditRendererComponent: BtnEditRenderedComponent
+  };
   formLayersPermits: FormGroup;
   layersPermitsToEdit;
   layersPermitsID = -1;
@@ -23,6 +26,7 @@ export class LayersPermitsFormComponent implements OnInit {
     private activatedRoute: ActivatedRoute,
     private router: Router,
     private cartographyGroupService: CartographyGroupService,
+    private roleService: RoleService,
     private http: HttpClient,
     private utils: UtilsService,
     ) {
@@ -59,6 +63,35 @@ export class LayersPermitsFormComponent implements OnInit {
 
     });
 
+
+
+    this.columnDefs = [
+      {
+        headerName: '',
+        checkboxSelection: true,
+        headerCheckboxSelection: true,
+        editable: false,
+        filter: false,
+        width: 40,
+        lockPosition:true,
+      },
+      {
+        headerName: '',
+        field: 'id',
+        editable: false,
+        filter: false,
+        width: 40,
+        lockPosition:true,
+        cellRenderer: 'btnEditRendererComponent',
+        cellRendererParams: {
+          clicked: this.newDataRole.bind(this)
+        },
+      },
+      { headerName: 'ID',  field: 'id', editable: false},
+      { headerName: this.utils.getTranslate('roleEntity.name'),  field: 'name'},
+      { headerName: this.utils.getTranslate('roleEntity.note'),  field: 'description' },
+     // { headerName: this.utils.getTranslate('application'),  field: 'application' },
+    ];
   }
 
 
@@ -102,4 +135,37 @@ export class LayersPermitsFormComponent implements OnInit {
   }
 
 
+  // AG GRID
+
+       /*
+      Important! Aquesta és la funció que li passarem al data grid a través de l'html per obtenir les files de la taula,
+      de moment no he trobat cap altre manera de que funcioni sense posar la nomenclatura = () =>,
+      pel que de moment hem dit de deixar-ho així!
+    */
+    getAllRoles = () => {
+    return this.roleService.getAll();
+  };
+  
+    /*Les dues funcions que venen ara s'activaran quan es cliqui el botó de remove o el de new a la taula,
+      si volguessim canviar el nom de la funció o qualsevol cosa, cal mirar l'html, allà es on es crida la funció
+      corresponent!
+    */
+  
+  removeDataRole( data: Role[])
+  {
+    console.log(data);
+  }
+  
+  newDataRole(id: any)
+  {
+    this.router.navigate(['role', id, 'roleForm']);
+  }
+  
+  applyChangesRole( data: Role[])
+  {
+        console.log(data);
+  }
+
 }
+
+

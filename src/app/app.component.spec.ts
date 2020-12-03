cf8a1f18e43be9747306f22c746d3e791f1372f4
gon-nexus <ggaliano@nexusgeographics.com>
2020-12-03 09:36:54 +0100

    app component spec arreglat

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -39,10 +39,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('admin-app');
   });
 
-  // it('should render title', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.debugElement.nativeElement;
-  //   expect(compiled.querySelector('.content span').textContent).toContain('admin-app app is running!');
-  // });
+  it('AppComponent should render title in a h1 tag', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!!');
+  });
 });

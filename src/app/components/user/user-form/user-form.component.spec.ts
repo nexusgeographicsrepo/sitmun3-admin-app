b055e77068ec1a8e65db600dd4fe834927addbad
gon-nexus <ggaliano@nexusgeographics.com>
2020-12-03 14:01:55 +0100

    formulari users fet a falta de les taules

--- src/app/components/user/user-form/user-form.component.spec.ts
+++ src/app/components/user/user-form/user-form.component.spec.ts
@@ -4,7 +4,11 @@ import { UserFormComponent } from './user-form.component';
 import { RouterModule } from '@angular/router';
 import { MaterialModule } from '../../../material-module';
 import { MatIconTestingModule } from '@angular/material/icon/testing';
-
+import { UserService } from 'dist/sitmun-frontend-core/public_api';
+import { ExternalConfigurationService } from 'src/app/ExternalConfigurationService';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { SitmunFrontendGuiModule } from 'dist/sitmun-frontend-gui/';
+import { RouterTestingModule } from '@angular/router/testing';
 describe('UserFormComponent', () => {
   let component: UserFormComponent;
   let fixture: ComponentFixture<UserFormComponent>;
@@ -12,7 +16,10 @@ describe('UserFormComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ UserFormComponent ],
-      imports: [ RouterModule.forRoot([]), MaterialModule, MatIconTestingModule]
+      imports: [HttpClientTestingModule, SitmunFrontendGuiModule, RouterTestingModule,
+         RouterModule.forRoot([]), MaterialModule, MatIconTestingModule],
+      providers: [UserService,
+        { provide: 'ExternalConfigurationService', useClass: ExternalConfigurationService }, ]
     })
     .compileComponents();
   });
@@ -27,3 +34,4 @@ describe('UserFormComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+ 

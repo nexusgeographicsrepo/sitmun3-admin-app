d4aa92a12843e4b2d24bed3d9d16f9d26d9c4873
gon-nexus <ggaliano@nexusgeographics.com>
2020-12-07 08:57:58 +0100

    Error specs task group corretgit

--- src/app/components/task-group/task-group-form/task-group-form.component.spec.ts
+++ src/app/components/task-group/task-group-form/task-group-form.component.spec.ts
@@ -5,7 +5,7 @@ import { RouterModule } from '@angular/router';
 import { MaterialModule } from '../../../material-module';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { MatIconTestingModule } from '@angular/material/icon/testing';
-import { TaskGroupService } from 'dist/sitmun-frontend-core/public_api';
+import { TaskGroupService } from 'dist/sitmun-frontend-core/';
 import { ExternalConfigurationService } from 'src/app/ExternalConfigurationService';
 import { HttpClientModule } from '@angular/common/http';
 import { SitmunFrontendGuiModule } from 'dist/sitmun-frontend-gui/';

a8986a056fd4e92c6247d985741610acdd6e0252
gon-nexus <ggaliano@nexusgeographics.com>
2020-12-14 10:24:15 +0100

    Tasques document,consulta i descarrega fetes

--- src/app/components/tasks-consultation/tasks-consultation.component.ts
+++ src/app/components/tasks-consultation/tasks-consultation.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { UtilsService } from '../../services/utils.service';
+import { BtnEditRenderedComponent } from 'dist/sitmun-frontend-gui/';
+import { Router } from '@angular/router';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-tasks-consultation',
@@ -7,9 +11,72 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TasksConsultationComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  columnDefs: any[];
+  public frameworkComponents = {
+    btnEditRendererComponent: BtnEditRenderedComponent
+  };
+
+
+  constructor(private utils: UtilsService,
+              private router: Router,
+              )
+              { }
+
+
+  ngOnInit()  {
+
+    this.columnDefs = [
+      {
+        headerName: '',
+        checkboxSelection: true,
+        headerCheckboxSelection: true,
+        editable: false,
+        filter: false,
+        width: 105,
+        lockPosition:true,
+      },
+      {
+        headerName: '',
+        field: 'id',
+        editable: false,
+        filter: false,
+        width: 120,
+        lockPosition:true,
+        cellRenderer: 'btnEditRendererComponent',
+        cellRendererParams: {
+          clicked: this.newData.bind(this)
+        },
+      },
+      { headerName: 'ID',  field: 'id', editable: false},
+      { headerName: this.utils.getTranslate('tasksConsultationEntity.name'),  field: 'name'},
+      { headerName: this.utils.getTranslate('tasksConsultationEntity.task'),  field: 'task'},
+      { headerName: this.utils.getTranslate('tasksConsultationEntity.informationType'),  field: 'informationType' },
+      { headerName: this.utils.getTranslate('tasksConsultationEntity.accesType'),  field: 'accesType' },
+      { headerName: this.utils.getTranslate('tasksConsultationEntity.command'),  field: 'command' },
+      { headerName: this.utils.getTranslate('tasksConsultationEntity.connection'),  field: 'connection' },
+      { headerName: this.utils.getTranslate('tasksConsultationEntity.associatedLayer'),  field: 'associatedLayer' }
+    ];
+  }
+
+  getAllTasksQuery = () => {
+    const aux:Array<any> = [];
+    return of(aux);
+  }
+
+  removeData( data: any[])
+  {
+    console.log(data);
+  }
+  
+  newData(id: any)
+  {
+    // this.router.navigate(['territory', id, 'territoryForm']);
+  }
+  
+  applyChanges( data: any[])
+  {
+        console.log(data);
   }
 
 }
--- src/app/components/tasks-download/tasks-download.component.ts
+++ src/app/components/tasks-download/tasks-download.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Territory } from 'dist/sitmun-frontend-core/territory/territory.model';
-import { TerritoryService } from 'dist/sitmun-frontend-core/';
 import { UtilsService } from '../../services/utils.service';
 import { BtnEditRenderedComponent } from 'dist/sitmun-frontend-gui/';
 import { Router } from '@angular/router';
@@ -19,8 +17,7 @@ export class TasksDownloadComponent implements OnInit {
   };
 
 
-  constructor(public territoryService: TerritoryService,
-              private utils: UtilsService,
+  constructor(private utils: UtilsService,
               private router: Router,
               )
               { }
@@ -53,39 +50,30 @@ export class TasksDownloadComponent implements OnInit {
       { headerName: 'ID',  field: 'id', editable: false},
       { headerName: this.utils.getTranslate('tasksDownloadEntity.name'),  field: 'name'},
       { headerName: this.utils.getTranslate('tasksDownloadEntity.observations'),  field: 'observations'},
-      { headerName: this.utils.getTranslate('tasksDownloadEntity.application'),  field: 'application', }
+      { headerName: this.utils.getTranslate('tasksDownloadEntity.application'),  field: 'application' }
     ];
   }
 
 
-    /*
-    Important! Aquesta és la funció que li passarem al data grid a través de l'html per obtenir les files de la taula,
-    de moment no he trobat cap altre manera de que funcioni sense posar la nomenclatura = () =>,
-    pel que de moment hem dit de deixar-ho així!
-  */
-  getAllTerritories = () => {
+
+  getAllTasksDownload = () => {
     const aux:Array<any> = [];
     return of(aux);
   }
 
-  /*Les dues funcions que venen ara s'activaran quan es cliqui el botó de remove o el de new a la taula,
-    si volguessim canviar el nom de la funció o qualsevol cosa, cal mirar l'html, allà es on es crida la funció
-    corresponent!
-  */
-
-removeData( data: Territory[])
-{
-  console.log(data);
-}
-
-newData(id: any)
-{
-  // this.router.navigate(['territory', id, 'territoryForm']);
-}
-
-applyChanges( data: Territory[])
-{
-      console.log(data);
-}
+  removeData( data: any[])
+  {
+    console.log(data);
+  }
+  
+  newData(id: any)
+  {
+    // this.router.navigate(['territory', id, 'territoryForm']);
+  }
+  
+  applyChanges( data: any[])
+  {
+        console.log(data);
+  }
 
 }

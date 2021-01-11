2187e7f27402d9288482c7d84d2c890dd48f2259
gon-nexus <ggaliano@nexusgeographics.com>
2021-01-11 17:01:20 +0100

    prova travis 10

--- src/app/components/shared/toolbar/toolbar.component.spec.ts
+++ src/app/components/shared/toolbar/toolbar.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ToolbarComponent } from './toolbar.component';
 import { MaterialModule } from '../../../material-module';
-import { AuthService, LoginService, Principal } from 'dist/sitmun-frontend-core/';
+import { LoginService, AuthService, Principal } from 'dist/sitmun-frontend-core/';
 import { ExternalConfigurationService } from 'src/app/ExternalConfigurationService';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MatIconTestingModule } from '@angular/material/icon/testing';
@@ -15,8 +15,8 @@ describe('ToolbarComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ ToolbarComponent ],
-      imports: [MaterialModule],      
-      providers: [HttpClientTestingModule,HttpClientModule, RouterTestingModule, SitmunFrontendGuiModule, MatIconTestingModule, LoginService, AuthService, Principal,
+      imports: [MaterialModule, HttpClientTestingModule,HttpClientModule, RouterTestingModule, SitmunFrontendGuiModule, MatIconTestingModule,],      
+      providers: [  LoginService, AuthService, Principal,
         { provide: 'ExternalConfigurationService', useClass: ExternalConfigurationService }, ]
     })
     .compileComponents();

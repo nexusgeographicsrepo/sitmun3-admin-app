2b212b13ab950baa518c5fdcf7cd0a118eeb80bf
gon-nexus <ggaliano@nexusgeographics.com>
2021-01-21 16:40:07 +0100

    Adaptat capes de fons al nou format

--- src/app/components/background-layers/background-layers-form/background-layers-form.component.ts
+++ src/app/components/background-layers/background-layers-form/background-layers-form.component.ts
@@ -121,7 +121,7 @@ export class BackgroundLayersFormComponent implements OnInit {
     })
     console.log(this.backgroundForm.value);
 
-    this.backgroundService.create(this.backgroundForm.value)
+    this.backgroundService.save(this.backgroundForm.value)
       .subscribe(resp => {
         console.log(resp);
         // this.router.navigate(["/company", resp.id, "formConnection"]);
@@ -144,7 +144,7 @@ export class BackgroundLayersFormComponent implements OnInit {
     this.backgroundToEdit.cartographyGroupName=cartographyGroup.name;
     this.backgroundToEdit.cartographyGroup=cartographyGroup
     this.backgroundToEdit.active= this.backgroundForm.value.active;
-    this.backgroundService.update(this.backgroundToEdit)
+    this.backgroundService.save(this.backgroundToEdit)
       .subscribe(resp => {
         console.log(resp);
       });
@@ -153,10 +153,23 @@ export class BackgroundLayersFormComponent implements OnInit {
 
 
   onSaveButtonClicked(){
+    let cartographyGroup= this.permissionGroups.find(x => x.id===this.backgroundForm.value.cartographyGroup )
+    this.backgroundForm.patchValue({
+      cartographyGroup : cartographyGroup
+    })
+    this.backgroundService.save(this.backgroundForm.value)
+      .subscribe(resp => {
+        console.log(resp);
+        this.backgroundToEdit=resp;
+      },
+      error=>{
+        console.log("error")
+      });
 
-    if(this.backgroundID !==-1)
-    {this.updateBackground();}
-    else {this.addNewBackground()}
+      this.backgroundForm.patchValue({
+        cartographyGroup : cartographyGroup.id
+      }) 
 
     }
+
 }

34326e56b515a1401d75aee0f51317b1dc65cda9
gon-nexus <ggaliano@nexusgeographics.com>
2021-01-26 12:53:52 +0100

    tree form update

--- src/app/components/trees/trees-form/trees-form.component.ts
+++ src/app/components/trees/trees-form/trees-form.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { TreeService, Tree} from '@sitmun/frontend-core'
+import { TreeService, TreeNodeService, CartographyService, Tree, TreeNode, Cartography } from '@sitmun/frontend-core';
 import { HttpClient } from '@angular/common/http';
 import { UtilsService } from '../../../services/utils.service';
 import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 import { Observable, of, Subject } from 'rxjs';
+import { DialogMessageComponent } from 'dist/sitmun-frontend-gui/';
+import { MatDialog } from '@angular/material/dialog';
 @Component({
   selector: 'app-trees-form',
   templateUrl: './trees-form.component.html',
@@ -13,24 +16,36 @@ import { Observable, of, Subject } from 'rxjs';
 })
 export class TreesFormComponent implements OnInit {
 
+  themeGrid: any = environment.agGridTheme;
   treeID: number = -1;
   treeForm: FormGroup;
   treeNodeForm: FormGroup;
+  idFictitiousCounter=-1;
   treeToEdit: Tree;
   dataLoaded: Boolean = false;
+  currentNodeIsFolder: Boolean;
+  newElement: Boolean = false;
   sendNodeUpdated: Subject<any> = new Subject <any>();
   getAllElementsNodes: Subject<boolean> = new Subject <boolean>();
+  createNodeEvent: Subject<boolean> = new Subject <boolean>();
+  getAllElementsEventCartographies: Subject<boolean> = new Subject <boolean>();
+  columnDefsCartographies: any[];
   constructor(
     private activatedRoute: ActivatedRoute,
     private router: Router,
     private treeService: TreeService,
+    private treeNodeService: TreeNodeService,
+    private cartographyService: CartographyService,
     private http: HttpClient,
     private utils: UtilsService,
+    public dialog: MatDialog,
   ) {
    
     this.initializeTreesForm();
     this.initializeTreesNodeForm();
   }
+
+
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(params => {
@@ -56,6 +71,23 @@ export class TreesFormComponent implements OnInit {
         );
       }
     });
+
+
+    this.columnDefsCartographies = [
+      {
+        headerName: '',
+        checkboxSelection: true,
+        headerCheckboxSelection: false,
+        editable: false,
+        filter: false,
+        minWidth: 45,
+        maxWidth: 45,
+        lockPosition: true
+      },
+      { headerName: 'Id', field: 'id', editable: false },
+      { headerName: this.utils.getTranslate('treesEntity.name'), field: 'name', editable: false },
+    ];
+
   }
 
   initializeTreesForm(): void {
@@ -73,11 +105,22 @@ export class TreesFormComponent implements OnInit {
       tooltip: new FormControl(null, []),
       cartography: new FormControl(null, []),
       radio: new FormControl(null, []),
-      visible: new FormControl(null, []),
+      active: new FormControl(null, []),
       _links: new FormControl(null, []),
       children: new FormControl(null, []),
+      parent: new FormControl(null, []),
+      isFolder: new FormControl(null, []),
+      type: new FormControl(null, []),
+      status: new FormControl(null, []),
+
 
     })
+  }
+
+  getAllCartographies = (): Observable<any> => {
+
+   return this.cartographyService.getAll();
+
   }
 
   getAllTreeNodes= (): Observable<any> => {
@@ -94,37 +137,192 @@ export class TreesFormComponent implements OnInit {
 
   nodeReceived(node)
   {
+    this.newElement=false;
+    let currentType;
+    if (node.isFolder)
+    {
+      this.currentNodeIsFolder = true;
+      currentType='folder'
+    }
+    else
+    {
+      this.currentNodeIsFolder = false;
+      currentType='node'
+    }
     console.log(node);
     this.treeNodeForm.patchValue({
       id: node.id,
       name: node.name,
       tooltip: node.tooltip,
-      cartography: null,
       radio: node.radio,
-      visible: node.visible,
+      active: node.active,
       _links: node._links,
-      children: node.children
+      children: node.children,
+      parent: node.parent,
+      isFolder: node.isFolder,
+      status: "Modified",
+      type:currentType
     })
   }
 
-  
-  onSaveButtonClicked(){ //TODO CHANGE TO UPDATE BUTTON WHEN AVAILABLE (Discomment the first line and delete line "sendNodeUpdated")
-    // this.getAllElementsNodes.next(true);
-    this.sendNodeUpdated.next(this.treeNodeForm.value)
-
-
-  }
-
-  onUpdateButtonClicked(){
-    this.sendNodeUpdated.next(this.treeNodeForm.value)
-  }
-
-  
-
-  getAllNodes(data)
+  createNode(parent)
   {
+    this.treeNodeForm.reset();
+    this.newElement=true;
+    this.currentNodeIsFolder=false
+    console.log(parent);
+    this.treeNodeForm.patchValue({
+      parent: parent.id,
+      isFolder: false,
+      children: [],
+      status: "Pending creation"
+    })
 
-    console.log(data);
   }
 
+  createFolder(parent)
+  {
+    this.treeNodeForm.reset();
+    this.newElement=true;
+    this.currentNodeIsFolder=true
+    this.treeNodeForm.patchValue({
+      parent: parent.id,
+      isFolder: true,
+      children: [],
+      status: "Pending creation"
+    })
+
+  }
+
+
+
+  
+  onSaveButtonClicked(){ 
+    this.getAllElementsNodes.next(true);
+  }
+
+  updateNode(){
+    console.log(this.treeNodeForm.value);
+    this.sendNodeUpdated.next(this.treeNodeForm.value)
+  }
+
+  onSaveFormButtonClicked(){
+    if(!this.currentNodeIsFolder) {this.getAllElementsEventCartographies.next(this.treeNodeForm.value)}
+    else { this.updateTreeLeft(null) }
+    
+  }
+
+  updateAllTrees(treesToUpdate: any[])
+  {
+    treesToUpdate.forEach(tree => {
+
+      if(tree.status)
+      {
+
+        if(status !== "Deleted")
+        {
+          let currentParent;
+          if(tree.parent !== null) {
+            currentParent= treesToUpdate.find(element => element.id === tree.parent ); 
+            currentParent.tree=this.treeToEdit;
+          }
+          else { currentParent=null;}
+          
+          if (currentParent !== undefined)
+          {
+            var treeNodeObj: TreeNode=new TreeNode();
+     
+            treeNodeObj.name= tree.name;
+            treeNodeObj.tooltip= tree.tooltip;
+            treeNodeObj.order= tree.order;
+            treeNodeObj.parent= currentParent;
+            treeNodeObj.active= tree.active;
+            treeNodeObj.cartography= tree.cartography;
+            treeNodeObj.tree= this.treeToEdit;
+            treeNodeObj._links= tree._links;
+            console.log(treeNodeObj)
+            if(treeNodeObj._links)
+            {
+              treeNodeObj._links.cartography.href=treeNodeObj._links.cartography.href.split("{")[0];
+              treeNodeObj._links.parent.href=treeNodeObj._links.parent.href.split("{")[0];
+              treeNodeObj._links.treeNode.href=treeNodeObj._links.treeNode.href.split("{")[0];
+              treeNodeObj.tree._links.allNodes.href=treeNodeObj.tree._links.allNodes.href.split("{")[0];
+            }
+      
+            console.log(treeNodeObj)
+            this.treeNodeService.save(treeNodeObj).subscribe(
+              result => {
+                console.log(result);
+              },
+              error => {
+                console.log(error);
+              }
+            )
+          }
+
+
+        }
+
+      }
+
+
+    });
+  }
+
+  
+
+  getAllNodes(data: TreeNode[])
+  {
+    console.log(data);
+    this.treeService.save( this.treeForm.value)
+    .subscribe(resp => {
+      this.treeToEdit=resp;
+      this.updateAllTrees(data);
+    },
+    error=>{
+      console.log(error);
+    });
+
+    
+  }
+
+  getSelectedRowsCartographies(data: any[] )
+  {
+    if(data.length<=0 && !this.currentNodeIsFolder)
+     {
+      const dialogRef = this.dialog.open(DialogMessageComponent);
+      dialogRef.componentInstance.title=this.utils.getTranslate("Error");
+      dialogRef.componentInstance.message=this.utils.getTranslate("cartographyNonSelectedMessage");
+       dialogRef.afterClosed().subscribe();
+     }
+    else {
+      this.updateTreeLeft(data[0])
+    }
+
+  }
+
+  updateTreeLeft(cartography)
+  {
+    this.treeNodeForm.patchValue({
+      cartography: cartography
+    })
+  
+    if(this.newElement) {
+      this.treeNodeForm.patchValue({
+        id: this.idFictitiousCounter
+      })
+      console.log(this.treeNodeForm.value);
+      this.idFictitiousCounter--;
+      this.createNodeEvent.next(this.treeNodeForm.value);
+    }
+    else{ 
+      this.updateNode() 
+    }
+
+    console.log(this.treeNodeForm.value);
+    this.newElement=false;
+    this.currentNodeIsFolder=undefined;
+    this.treeNodeForm.reset();
+  }
 }
+

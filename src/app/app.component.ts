08c493938090622b5c9ecdb1f9341e21f8736bc3
gon-nexus <ggaliano@nexusgeographics.com>
2021-03-18 10:00:46 +0100

    languages fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -73,56 +73,56 @@ export class AppComponent {
         this.currentAccount = account;
       });
     }
-    this.loadLanguages();
+    // this.loadLanguages();
   }
 
-  //Load from server all languages that we will use
-  async loadLanguages(){
-    let catalanLanguage = null;
-    let spanishLanguage = null;
-    let englishLanguage = null;
-    this.languageService.getAll().subscribe(
-      async result => {
-        console.log(result);
-        result.forEach(language => {
-          if(language.name == 'Catala') { catalanLanguage= language }
-          if(language.name == 'Español') { spanishLanguage= language }
-          if(language.name == 'English') { englishLanguage= language }
-        });
+  // //Load from server all languages that we will use
+  // async loadLanguages(){
+  //   let catalanLanguage = null;
+  //   let spanishLanguage = null;
+  //   let englishLanguage = null;
+  //   this.languageService.getAll().subscribe(
+  //     async result => {
+  //       console.log(result);
+  //       result.forEach(language => {
+  //         if(language.name == 'Catala') { catalanLanguage= language }
+  //         if(language.name == 'Español') { spanishLanguage= language }
+  //         if(language.name == 'English') { englishLanguage= language }
+  //       });
 
-        if(catalanLanguage != null){
-          config.languagesObjects.catalan=catalanLanguage;
-        }
-        else{
-          let catalanLanguageObj = new Language();
-          catalanLanguageObj.name= 'Catala';
-          catalanLanguageObj.shortname= 'ca';
-          console.log( await this.languageService.save(catalanLanguageObj).toPromise() );
-        }
+  //       if(catalanLanguage != null){
+  //         config.languagesObjects.catalan=catalanLanguage;
+  //       }
+  //       else{
+  //         let catalanLanguageObj = new Language();
+  //         catalanLanguageObj.name= 'Catala';
+  //         catalanLanguageObj.shortname= 'ca';
+  //         console.log( await this.languageService.save(catalanLanguageObj).toPromise() );
+  //       }
 
-        if(spanishLanguage != null){
-          config.languagesObjects.spanish=spanishLanguage;
-        }
-        // else{
-        //   let spanishLanguageObj = new Language();
-        //   spanishLanguageObj.name= 'Español';
-        //   spanishLanguageObj.shortname= 'spa';
-        //   config.languagesObjects.spanish= await this.languageService.save(spanishLanguageObj).toPromise();
-        // }
+  //       if(spanishLanguage != null){
+  //         config.languagesObjects.spanish=spanishLanguage;
+  //       }
+  //       // else{
+  //       //   let spanishLanguageObj = new Language();
+  //       //   spanishLanguageObj.name= 'Español';
+  //       //   spanishLanguageObj.shortname= 'spa';
+  //       //   config.languagesObjects.spanish= await this.languageService.save(spanishLanguageObj).toPromise();
+  //       // }
 
-        if(englishLanguage != null){
-          config.languagesObjects.english=englishLanguage;
-        }
-        else{
-          let englishLanguageObj = new Language();
-          englishLanguageObj.name= 'English';
-          englishLanguageObj.shortname= 'en';
-          config.languagesObjects.english= await this.languageService.save(englishLanguageObj).toPromise()
-        }
+  //       if(englishLanguage != null){
+  //         config.languagesObjects.english=englishLanguage;
+  //       }
+  //       else{
+  //         let englishLanguageObj = new Language();
+  //         englishLanguageObj.name= 'English';
+  //         englishLanguageObj.shortname= 'en';
+  //         config.languagesObjects.english= await this.languageService.save(englishLanguageObj).toPromise()
+  //       }
 
-      }
-    )
-  }
+  //     }
+  //   )
+  // }
 
 }
 
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component} from '@angular/core';
-import { AuthService } from 'dist/sitmun-frontend-core/';
+import { AuthService, LanguageService, Language } from 'dist/sitmun-frontend-core/';
 import { LoginService } from 'dist/sitmun-frontend-core/';
 import { environment } from 'src/environments/environment';
 import { config } from 'src/config';
@@ -28,6 +28,7 @@ export class LoginComponent {
     constructor( private fb: FormBuilder,
         private authService: AuthService,
         private loginService: LoginService,
+        private languageService: LanguageService,
         private router: Router,
         private trans: TranslateService ) {
         
@@ -54,6 +55,7 @@ export class LoginComponent {
                 this.translate.use(this.form.value.lang)
                 this.translate.setDefaultLang(this.form.value.lang);
                 localStorage.setItem('lang',this.form.value.lang );
+                this.loadLanguages();
                 console.log( 'User is logged in' );
                 this.router.navigateByUrl( '/' );
             }, ( err ) => {
@@ -63,4 +65,52 @@ export class LoginComponent {
 
         }
     }
+
+      //Load from server all languages that we will use
+  async loadLanguages(){
+    let catalanLanguage = null;
+    let spanishLanguage = null;
+    let englishLanguage = null;
+    this.languageService.getAll().subscribe(
+      async result => {
+        console.log(result);
+        result.forEach(language => {
+          if(language.name == 'Catala') { catalanLanguage= language }
+          if(language.name == 'Español') { spanishLanguage= language }
+          if(language.name == 'English') { englishLanguage= language }
+        });
+
+        if(catalanLanguage != null){
+          config.languagesObjects.catalan=catalanLanguage;
+        }
+        else{
+          let catalanLanguageObj = new Language();
+          catalanLanguageObj.name= 'Catala';
+          catalanLanguageObj.shortname= 'ca';
+          console.log( await this.languageService.save(catalanLanguageObj).toPromise() );
+        }
+
+        if(spanishLanguage != null){
+          config.languagesObjects.spanish=spanishLanguage;
+        }
+        // else{
+        //   let spanishLanguageObj = new Language();
+        //   spanishLanguageObj.name= 'Español';
+        //   spanishLanguageObj.shortname= 'spa';
+        //   config.languagesObjects.spanish= await this.languageService.save(spanishLanguageObj).toPromise();
+        // }
+
+        if(englishLanguage != null){
+          config.languagesObjects.english=englishLanguage;
+        }
+        else{
+          let englishLanguageObj = new Language();
+          englishLanguageObj.name= 'English';
+          englishLanguageObj.shortname= 'en';
+          config.languagesObjects.english= await this.languageService.save(englishLanguageObj).toPromise()
+        }
+
+      }
+    )
+  }
 }

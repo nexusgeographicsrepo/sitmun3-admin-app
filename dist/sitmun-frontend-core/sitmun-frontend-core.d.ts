37009d6ea28266b46a795f720ddb9796cbc4b213
gon-nexus <ggaliano@nexusgeographics.com>
2021-04-19 14:12:35 +0200

    Fix travis 1

--- dist/sitmun-frontend-core/sitmun-frontend-core.d.ts
+++ dist/sitmun-frontend-core/sitmun-frontend-core.d.ts
@@ -2,8 +2,7 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public_api';
-export { ResourceService as ɵa } from './angular-hal/src/lib/resource.service';
-export { HasAnyAuthorityOnTerritoryDirective as ɵc } from './auth/has-any-authority-on-territory.directive';
-export { HasAnyAuthorityDirective as ɵb } from './auth/has-any-authority.directive';
+export { HasAnyAuthorityOnTerritoryDirective as ɵb } from './auth/has-any-authority-on-territory.directive';
+export { HasAnyAuthorityDirective as ɵa } from './auth/has-any-authority.directive';
 
 //# sourceMappingURL=sitmun-frontend-core.d.ts.map

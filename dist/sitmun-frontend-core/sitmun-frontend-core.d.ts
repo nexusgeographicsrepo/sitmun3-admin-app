41d19eaa28d17ceaf7f1159747ed26f9f183e381
gon-nexus <ggaliano@nexusgeographics.com>
2021-04-21 14:18:52 +0200

    Gitignore issue fixed

--- dist/sitmun-frontend-core/sitmun-frontend-core.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-export * from './public_api';
-export { HasAnyAuthorityOnTerritoryDirective as ɵb } from './auth/has-any-authority-on-territory.directive';
-export { HasAnyAuthorityDirective as ɵa } from './auth/has-any-authority.directive';
-
-//# sourceMappingURL=sitmun-frontend-core.d.ts.map

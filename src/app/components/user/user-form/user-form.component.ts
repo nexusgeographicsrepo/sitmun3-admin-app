e2589bb3544f510f4e9c7e4f764b77c2b516c413
gon-nexus <ggaliano@nexusgeographics.com>
2021-06-15 13:55:39 +0200

    added type column on user position

--- src/app/components/user/user-form/user-form.component.ts
+++ src/app/components/user/user-form/user-form.component.ts
@@ -149,6 +149,7 @@ export class UserFormComponent implements OnInit {
       this.utils.getEditableColumnDef('userEntity.position', 'name'),
       this.utils.getEditableColumnDef('userEntity.organization', 'organization'),
       this.utils.getEditableColumnDef('userEntity.mail', 'email'),
+      this.utils.getEditableColumnDef('userEntity.type', 'type'),
       this.utils.getDateColumnDef('userEntity.expirationDate', 'expirationDate',true),
       this.utils.getDateColumnDef('userEntity.dataCreated', 'createdDate'),
       this.utils.getStatusColumnDef()
